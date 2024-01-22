ce1f4312198ec23ccb94cfa8fe4dd72612aac4ba
Divya120700 <155365458+Divya120700@users.noreply.github.com>
2024-01-22 14:42:45 +0530

    Update system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -3,3 +3,30 @@ CPU_INFO=$(lscpu)
 MEMORY_INFO=$(du -h)
 DATE_INFO=$(date)
 DISK_INFO=$(df -h)
+
+# Create an HTML file with captured information
+cat <<EOF > /usr/share/nginx/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>System Information</title>
+</head>
+<body>
+    <h1>System Information</h1>
+    <pre>
+    CPU Information:
+    $CPU_INFO
+
+    Memory Information:
+    $MEMORY_INFO
+
+    Date and Time:
+    $DATE_INFO
+
+    Disk Space:
+    $DISK_INFO
+    </pre>
+</body>
+</html>
+EOF
+
